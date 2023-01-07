9d7c5ef39e7b93a7458d0cf9efe2395a096dab48
Sonalika Dixit <109176731+dixitsona22@users.noreply.github.com>
2023-01-07 23:19:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 /** @format */
-
 let arr = [
   { id: 1, name: "john", age: "18", profession: "developer" },
   { id: 2, name: "jack", age: "20", profession: "developer" },
@@ -7,21 +6,45 @@ let arr = [
 ];
 
 function PrintDeveloperswithMap() {
-  //Write your code here
+  const itemYouWant = array.map( (item) => {
+    if (item.profession === 'developer') {
+       console.log(itemYouWant);
+    }
+ });   
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here
+  arr.forEach( (item) => {
+    if (item.profession === 'developer') {
+          console.log(item);
+    }
+  });
 }
 
 function addData() {
   //Write your code here
-}
+    var b =   {id:4,name:"susan",age:"20",profession:"intern"};
+    arr.push(b);   
+    console.log(arr);
+ }
 
 function removeAdmin() {
   //Write your code here
+  
 }
-
 function concatenateArray() {
   //Write your code here
+  let arr1 = [
+  { id: 4, name: "jay", age: "40", profession: "developer" },
+  { id: 5, name: "shyam", age: "26", profession: "intern" },
+  { id: 6, name: "ram", age: "43", profession: "intern" },
+];
+  var a = arr1[0];
+  var b = arr1[1];
+  var c = arr[2];
+  arr.push(a);
+  arr.push(b);
+  arr.push(c);
+  console.log(arr);
 }
